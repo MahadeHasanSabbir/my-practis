9cabf7493201f469e6d15d727dfa9ac8e83d2f3c
Mahade Hasan Sabbir <61137327+MahadeHasanSabbir@users.noreply.github.com>
2021-07-10 08:16:46 +0600

    Add files via upload

---
+++ AlgorithmCode/lab41.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Marge function
+void merge(int array[], int l, int m, int h)
+{
+    int arr1[m],arr2[m];
+    int n1,n2,i,j,k = l;
+    n1 = m - l + 1;
+    n2 = h - m;
+    //create two sub array
+    for(i = 0; i < n1; i++) {
+        arr1[i] = array[l+i];
+    }
+    for(j = 0; j < n2; j++) {
+        arr2[j] = array[m+j+1];
+    }
+    //merge sub array
+    i = 0;
+    j = 0;
+    while(k <= h){
+        if (i < n1){
+            if (j >= n2) {
+                array[k++] = arr1[i++];
+            }
+            else if(arr1[i] >= arr2[j]){
+                array[k++] = arr1[i++];
+            }
+            else {
+                array[k++] = arr2[j++];
+            }
+        }
+        else {
+            array[k++] = arr2[j++];
+        }
+    }
+
+    return;
+}
+//Marge sort function
+void merge_sort(int Array[], int low, int high)
+{
+    if(low < high) {
+        int mid = (low + high) / 2;
+        merge_sort(Array, low, mid);
+        merge_sort(Array, mid + 1, high);
+        merge(Array, low, mid, high);
+    }
+
+    return;
+}
+//main function
+int main(void)
+{
+    int asize, i;
+    printf("\n Number of elements: ");
+    scanf("%d", &asize);
+    int elements[asize];
+    srand(time(0));
+    printf("\n The random numbers:");
+    //take random numbers as elements
+    for (i = 0; i < asize; i++) {
+        elements[i] = (rand() % 500);
+        printf("\t%d", elements[i]);
+    }
+    //Applying Merge sort
+    merge_sort(elements, 0, asize - 1);
+    //display elements
+    printf("\n After Merge sort: ");
+    for (i = 0; i < asize; i++){
+        printf("\t%d", elements[i]);
+    }
+
+    return 0;
+}
---
+++ AlgorithmCode/lab42.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Partition for quick sort function
+int partition(int array[], int p, int r)
+{
+    int lastnum = array[r];
+    int i = p - 1;
+    for (int j = p; j <= (r - 1); j++) {
+        if (array[j] >= lastnum) {
+            //swap element in descending order
+            int temp = array[++i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    //swap last element to proper place
+    int temp = array[++i];
+    array[i] = array[r];
+    array[r] = temp;
+
+    return i;
+}
+//Quick sort function
+void quicksort(int Array[], int point, int range)
+{
+    if (point < range) {
+        int partitionindex = partition(Array, point, range);
+        quicksort(Array, point, partitionindex - 1);
+        quicksort(Array, partitionindex + 1, range);
+    }
+
+    return;
+}
+//main function
+int main(void)
+{
+    int asize, i;
+    printf("\n Number of elements: ");
+    scanf("%d", &asize);
+    int elements[asize];
+    srand(time(0));
+    printf("\n The random numbers:");
+    //take random numbers as elements
+    for (int i = 0; i < asize; i++) {
+        elements[i] = (rand() % 500);
+        printf("\t%d", elements[i]);
+    }
+    //Applying Quick sort on elements
+    quicksort(elements, 0, asize - 1);
+    //display elements
+    printf("\n After Quick sort:  ");
+    for (int i = 0; i < asize; i++) {
+        printf("\t%d", elements[i]);
+    }
+
+    return 0;
+}
